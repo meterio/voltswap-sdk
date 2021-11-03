ec31ef928cf74e28a06200957b07dbc4f0baa365
philipappiah <pappiahkubi1@gmail.com>
2021-11-03 09:55:54 +0000

    add init_hash and factory routing

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -1,7 +1,11 @@
 import JSBI from 'jsbi'
-
+import invariant from 'tiny-invariant'
+import { Tags, TokenInfo } from '@uniswap/token-lists'
 import { SolidityType } from '../constants'
 import { validateSolidityTypeInstance } from '../utils'
+import { validateAndParseAddress } from '../utils'
+
+import { ChainId } from '../constants'
 
 /**
  * A currency is any fungible financial instrument on Ethereum, including Ether and all ERC20 tokens.
@@ -33,5 +37,83 @@ export class Currency {
   }
 }
 
-const ETHER = Currency.ETHER
+export class Token extends Currency {
+  public readonly chainId: ChainId
+  public readonly address: string
+
+  public constructor(chainId: ChainId, address: string, decimals: number, symbol?: string, name?: string) {
+    super(decimals, symbol, name)
+    this.chainId = chainId
+    this.address = validateAndParseAddress(address)
+  }
+
+  /**
+   * Returns true if the two tokens are equivalent, i.e. have the same chainId and address.
+   * @param other other token to compare
+   */
+  public equals(other: Token): boolean {
+    // short circuit on reference equality
+    if (this === other) {
+      return true
+    }
+    return this.chainId === other.chainId && this.address === other.address
+  }
+
+  /**
+   * Returns true if the address of this token sorts before the address of the other token
+   * @param other other token to compare
+   * @throws if the tokens have the same address
+   * @throws if the tokens are on different chains
+   */
+  public sortsBefore(other: Token): boolean {
+    invariant(this.chainId === other.chainId, 'CHAIN_IDS')
+    invariant(this.address !== other.address, 'ADDRESSES')
+    return this.address.toLowerCase() < other.address.toLowerCase()
+  }
+}
+
+/**
+ * Compares two currencies for equality
+ */
+export function currencyEquals(currencyA: Currency, currencyB: Currency): boolean {
+  if (currencyA instanceof Token && currencyB instanceof Token) {
+    return currencyA.equals(currencyB)
+  } else if (currencyA instanceof Token) {
+    return false
+  } else if (currencyB instanceof Token) {
+    return false
+  } else {
+    return currencyA === currencyB
+  }
+}
+
+
+type TagDetails = Tags[keyof Tags]
+
+export interface TagInfo extends TagDetails {
+  id: string
+}
+
+/**
+ * Token instances created from token info.
+ */
+export class WrappedTokenInfo extends Token {
+  public readonly tokenInfo: TokenInfo
+  public readonly tags: TagInfo[]
+
+  constructor(tokenInfo: TokenInfo, tags: TagInfo[]) {
+    super(tokenInfo.chainId, tokenInfo.address, tokenInfo.decimals, tokenInfo.symbol, tokenInfo.name)
+    this.tokenInfo = tokenInfo
+    this.tags = tags
+  }
+
+  public get logoURI(): string | undefined {
+    return this.tokenInfo.logoURI
+  }
+}
+
+const ETHER = new WrappedTokenInfo({chainId:ChainId.THETA, address:'0x4Dc08B15ea0e10B96c41Aec22fAB934bA15C983e', decimals:18, symbol:'WTFUEL', name:'Wrapped TFuel',
+logoURI:'https://raw.githubusercontent.com/meterio/token-list/master/data/TFUEL/logo.png'
+},
+[])
 export { ETHER }
